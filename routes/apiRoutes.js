6afb8878d0fdbf604dc34e8b3e86afd4e1d87ebb
jam <jaymee602@gmail.com>
2022-09-02 15:29:50 -0700

    add comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-const { del } = require('express/lib/application');
-const { json } = require('express/lib/response');
 const fs = require('fs');
 let notesArray = require('../db/db');
 
@@ -11,13 +9,17 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     // set id
     req.body.id = notesArray.length + 1;
+    // set text of new note = text on body of request
     let newNote = req.body;
+    // add new note to the notes array
     notesArray.push(newNote);
+    //update the database
     updateNotesArray();
     res.send(notesArray);
 });
 
 router.delete('/notes/:id', (req, res) => {
+    // iterate through notes array until we find the id = to the req id
     let noteToDelete = req.params.id;
     for (let i = 0; i < notesArray.length; i++) {
         if (noteToDelete == notesArray[i].id) {
@@ -25,6 +27,7 @@ router.delete('/notes/:id', (req, res) => {
             break;
         }
     }
+    // update the database
     updateNotesArray();
     res.json(notesArray);
 });
