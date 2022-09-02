4e2a0c729324a67549d179616cb9d9619f222a27
jam <jaymee602@gmail.com>
2022-09-02 15:26:22 -0700

    add delete request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const { del } = require('express/lib/application');
 const { json } = require('express/lib/response');
 const fs = require('fs');
-const notesArray = require('../db/db');
+let notesArray = require('../db/db');
 
 router.get('/notes', (req, res) => {
     res.json(notesArray);
@@ -16,10 +17,23 @@ router.post('/notes', (req, res) => {
     res.send(notesArray);
 });
 
+router.delete('/notes/:id', (req, res) => {
+    let noteToDelete = req.params.id;
+    for (let i = 0; i < notesArray.length; i++) {
+        if (noteToDelete == notesArray[i].id) {
+            notesArray.splice(i, 1);
+            break;
+        }
+    }
+    updateNotesArray();
+    res.json(notesArray);
+});
+
 function updateNotesArray () {
     fs.writeFileSync(
         'db/db.json',
         JSON.stringify(notesArray)
     )
-}
+};
+
 module.exports = router;
